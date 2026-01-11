dacec438b1e24c2e1dd6715740aa417b9d419abe
Marcos <marcospolo757@gmail.com>
2026-01-11 11:41:45 -0700

    added a comment

--- csharp-prep/Prep1/Program.cs
+++ csharp-prep/Prep1/Program.cs
@@ -4,6 +4,12 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep1 World!");
+        Console.WriteLine("What is your first name?");
+        string first_name = Console.ReadLine();
+        Console.WriteLine($"Your name is {first_name}");
+
+        Console.WriteLine("What is your second name?");
+        string second_name = Console.ReadLine();
+        Console.WriteLine($" Here your second name {second_name}?");
     }
 }
